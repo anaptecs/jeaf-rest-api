0fdf7812d019324bd5d9858746dbfcde717c563f
Tillmann Schall <tls@anaptecs.de>
2022-09-20 12:02:12 +0200

    Update documentation

--- jeaf-rest-request-executor-api/src/main/java/com/anaptecs/jeaf/rest/executor/api/ContentType.java
+++ jeaf-rest-request-executor-api/src/main/java/com/anaptecs/jeaf/rest/executor/api/ContentType.java
@@ -5,15 +5,33 @@
  */
 package com.anaptecs.jeaf.rest.executor.api;
 
+/**
+ * Enumeration defines the supported content types for REST calls
+ * 
+ * @author JEAF Development Team
+ */
 public enum ContentType {
   JSON("application/json"), XML("application/xml");
 
+  /**
+   * Mime type that belongs to the content type-
+   */
   private final String mimeType;
 
+  /**
+   * Initialize enumeration.
+   * 
+   * @param pMimeType Mime type the belong to the content type. The parameter must not be null.
+   */
   private ContentType( String pMimeType ) {
     mimeType = pMimeType;
   }
 
+  /**
+   * Method returns the mime type that belongs to this content type.
+   * 
+   * @return String Mime type. The method never returns null.
+   */
   public String getMimeType( ) {
     return mimeType;
   }
