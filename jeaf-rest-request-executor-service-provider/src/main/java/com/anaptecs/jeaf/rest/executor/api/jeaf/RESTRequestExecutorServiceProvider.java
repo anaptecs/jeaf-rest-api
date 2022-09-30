4c49f89f61f70fbb086fcd18401ac53f1ff7e2d3
JEAF Development Team <tls@anaptecs.de>
2022-09-30 08:31:10 +0200

    Added JEAF Service Provider to API

---
+++ jeaf-rest-request-executor-service-provider/src/main/java/com/anaptecs/jeaf/rest/executor/api/jeaf/RESTRequestExecutorServiceProvider.java
@@ -0,0 +1,17 @@
+/**
+ * anaptecs GmbH, Ricarda-Huch-Str. 71, 72760 Reutlingen, Germany
+ * 
+ * Copyright 2004 - 2022. All rights reserved.
+ */
+package com.anaptecs.jeaf.rest.executor.api.jeaf;
+
+import com.anaptecs.jeaf.core.api.ServiceProvider;
+import com.anaptecs.jeaf.rest.executor.api.RESTRequestExecutor;
+
+/**
+ * Interface enables {@link RESTRequestExecutor} to be used as JEAF Service Provider.
+ * 
+ * @author JEAF Development Team
+ */
+public interface RESTRequestExecutorServiceProvider extends ServiceProvider {
+}
